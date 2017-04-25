1b9e345783bc791dd92749a7462897a8aa92f0c0
MSDN-SPECIAL\Administrator <myeongseob91@naver.com>
2017-04-25 23:40:33 +0900

    start

---
+++ src/main/java/com/cppc/MyCppcApplication.java
@@ -0,0 +1,12 @@
+package com.cppc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCppcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCppcApplication.class, args);
+	}
+}
